c6e4457f40df5560dff01272feb47c38886df744
Chris Laughlin <bluntm@gmail.com>
2018-07-23 22:58:43 +0100

    Updating index with new prop type

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import isEven from './isEven';
-import isLessThan from './isLessThan';
-import isGreaterThan from './isGreaterThan';
-import isBetween from './isBetween';
-import includes from './includes';
-import isRequiredWhen from './isRequiredWhen';
+import isEven from "./isEven";
+import isLessThan from "./isLessThan";
+import isGreaterThan from "./isGreaterThan";
+import isBetween from "./isBetween";
+import includes from "./includes";
+import isRequiredWhen from "./isRequiredWhen";
+import isRequiredWhenAll from "./isRequiredWhenAll";
 
 export {
-    isEven,
-    isLessThan,
-    isGreaterThan,
-    isBetween,
-    includes,
-    isRequiredWhen
-}
+  isEven,
+  isLessThan,
+  isGreaterThan,
+  isBetween,
+  includes,
+  isRequiredWhen,
+  isRequiredWhenAll
+};
